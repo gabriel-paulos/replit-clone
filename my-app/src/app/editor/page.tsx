9a394edd94bc737f520c6d8f4dd750e683c3cb07
fooo <gabrielp7700@gmail.com>
2025-02-25 21:34:59 -0500

    start with editor

---
+++ my-app/src/app/editor/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Editor } from '@monaco-editor/react';
+
+function App() {
+
+    return (<Editor height="90vh" defaultLanguage='javascript' defaultValue='// some comment'>;)
+}
