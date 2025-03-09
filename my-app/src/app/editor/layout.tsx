885de66e981350f07434d1fe31c15743faef931b
fooo <gabrielp7700@gmail.com>
2025-03-09 18:59:03 -0400

    Combined the sidebar and the header successfully in editor page

--- my-app/src/app/editor/layout.tsx
+++ my-app/src/app/editor/layout.tsx
@@ -3,19 +3,12 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import { cookies } from "next/cookies";
 
-import { NavUser } from "./(components)/nav-user";
-
-import { SidebarInset, SidebarProvider, SidebarSeparator, SidebarTrigger, SidebarFooter, } from "@/components/ui/sidebar";
-
-import { Separator } from "@/components/ui/separator";
-
-import { AppSidebar } from "./(components)/master-sidebar";
+import { SidebarInset, SidebarProvider, SidebarSeparator, SidebarTrigger, SidebarFooter, } from "@/components/ui/sidebar"
 
 import { TestSidebar } from "./(components)/app-sidebar"; 
 
 import { EditorHeader } from "./(components)/site-header";
 
-import { ProfileForm } from "./(components)/formtest";
 
 import {
   Breadcrumb,
@@ -26,6 +19,7 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { Editor } from "@monaco-editor/react";
+import App from "next/app";
 
 
 const geistSans = Geist({
@@ -50,65 +44,28 @@ export default function DashboardLayout({
 }>) {
     return (
 
-/*
-<SidebarProvider
-      style={
-        {
-          "--sidebar-width": "350px",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar />
-      <SidebarInset>
-        <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">All Inboxes</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Inbox</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          {Array.from({ length: 24 }).map((_, index) => (
-            <div
-              key={index}
-              className="aspect-video h-12 w-full rounded-lg bg-muted/50"
-            />
-          ))}
-        </div>
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
-    */
-
-    /*
-      <SidebarProvider>
-        <TestSidebar />
-        <main>
-        <header className="sticky top-0 flex shrink-0 items-center justify-right gap-2 border-b bg-background p-4">
-          <div className="h-6 px-4 w-full flex items-center sticky justify-between top-0 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-            <div className="flex items-center space-x-4">
-              
-            </div>
-          </div>
-          </header>
-
+    <div className="[--header-height:calc(theme(spacing.14))]">
+      <SidebarProvider className="flex flex-col">
+        <EditorHeader/>
+        
+        <div className="flex flex-1">
+          <TestSidebar />
           <SidebarInset>
-
-          {children}
+          <div className="flex flex-1 flex-col gap-4 p-4">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+              </div>
+              <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+            </div>
           </SidebarInset>
-          
-        </main>
+        </div>
+      
       </SidebarProvider>
-      */
 
-    <EditorHeader/>
+    </div>
+
+    
   );
 }
